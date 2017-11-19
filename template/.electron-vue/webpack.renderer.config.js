8205f8403a6c0e56250d1834434758d1ac11c9d3
刘盼峰 <liupanfeng11@lonlife.cn>
2017-11-19 16:19:03 +0800

    Typescript修复，仍有遗留

--- template/.electron-vue/webpack.renderer.config.js
+++ template/.electron-vue/webpack.renderer.config.js
@@ -6,7 +6,7 @@ const path = require('path')
 const { dependencies } = require('../package.json')
 const webpack = require('webpack')
 
-const MinifyPlugin = require("babel-minify-webpack-plugin")
+const MinifyPlugin = require('babel-minify-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
